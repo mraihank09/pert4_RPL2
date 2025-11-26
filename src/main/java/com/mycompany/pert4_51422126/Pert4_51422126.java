2576514c5270817b682aaeb6b411528ec3c2c404
Raihan <mraihank0@gmail.com>
2025-11-26 18:48:45 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert4_51422126/Pert4_51422126.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_51422126;
+
+/**
+ *
+ * @author USER
+ */
+public class Pert4_51422126 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
